5066373ed883341dee4db5c80e6b7faff4674157
Heistoo <46348338+Heistoo@users.noreply.github.com>
2023-10-31 12:10:15 -0300

    add queries to reach table class

--- front/js/script.js
+++ front/js/script.js
@@ -6,5 +6,48 @@ const EndPoint = '/filmes'
 async function obterFilmes(){
     const URLcompleta = `${protocolo}${host}${EndPoint}`
     const filmes = (await axios.get(URLcompleta)).data
-    console.log(filmes)
+    //console.log(filmes)
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    for (let filme of filmes){
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+}
+async function cadastrarFilme(){
+    const URLcompleta = `${protocolo}${host}${EndPoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput') 
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    if(titulo && sinopse) {
+        
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
+    
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+    for(let filme of filmes){
+        console.log(filme.titulo, filme.sinopse)
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+        }
+    }
+    else{
+        let alert = document.querySelector('.alert')
+        alert.classList.add('show')
+        alert.classList.remove('d-none')
+        setTimeout(() => {
+            alert.classList.remove('show')
+            alert.classList.add('d-none')
+        }, 2000)
+    }
 }
